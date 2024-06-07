b85bdf6576e64727022d43135b6ace0dcbb61934
Hoang-An1109 <phamhoangan11092001@gmail.com>
2024-06-07 16:02:43 +0700

    update

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import controller.HomeController;
+import view.HomePage;
+import view.NewPage;
+import view.SortProductListMenu;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+//        HomeController homeController = new HomeController();
+//        homeController.load();
+        SortProductListMenu.sortView();
+    }
+}
